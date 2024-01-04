960723a5d1bd562f7e86c0335f9de42170ecd035
Therese Benz <tmbenz@my.canyons.edu>
2024-01-03 18:58:39 -0800

    Lesson 1

---
+++ lesson_one/Lessonone.java
@@ -0,0 +1,10 @@
+package lesson_one;
+
+public class Lessonone {
+    public static void main(String[] args) {
+        int x = 0;
+        int y = 5;
+        int sum = x + y;
+        System.out.println(sum);
+    }
+}
